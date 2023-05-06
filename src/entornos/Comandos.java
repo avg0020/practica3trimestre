146537a4c219c1cd341edef5c9ca8716c4552614
avg0020 <avg0020@alu.medac.es>
2023-05-06 19:17:18 +0200

    Cambios por comandos

---
+++ src/entornos/Comandos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entornos;
+
+/**
+ *
+ * @author pc
+ */
+public class Comandos {
+    /*
+        git init
+
+        git config user.name avg0020
+
+        git config user.email avg0020@alu.medac.es
+
+        git config user.pasword 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
+
+        git add.
+
+        git commit -m "Cambios por comandos"
+
+        git push -u origin master
+    
+    */
+}
